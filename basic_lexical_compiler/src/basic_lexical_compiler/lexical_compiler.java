ab92cf21481f4ad81088f24de5c7ee2b42a04f0a
tharindu99 <madushanka.best@gmail.com>
2017-07-20 09:08:41 +0530

    project initialize

---
+++ basic_lexical_compiler/src/basic_lexical_compiler/lexical_compiler.java
@@ -0,0 +1,11 @@
+package basic_lexical_compiler;
+
+public class lexical_compiler {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Lexical compiler..");
+		
+	}
+
+}
